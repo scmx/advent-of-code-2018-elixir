4464db8e7890185e4eb4cfdbc5977312ad22a5db
Albert Arvidsson <albert.arvidsson@gmail.com>
2018-12-01 06:55:30 +0100

    Add Day01 part 2 first_frequency_repeated_twice/1

--- lib/day_01_chronal_calibration.ex
+++ lib/day_01_chronal_calibration.ex
@@ -3,8 +3,41 @@ defmodule Adventofcode.Day01ChronalCalibration do
 
   def resulting_frequency(input) do
     input
+    |> frequencies
+    |> Enum.sum()
+  end
+
+  def first_frequency_repeated_twice(input) do
+    input
+    |> frequencies
+    |> do_first_frequency_repeated_twice
+  end
+
+  defp frequencies(input) do
+    input
     |> String.split(~r/\n|, /)
     |> Enum.map(&String.to_integer/1)
-    |> Enum.sum()
+  end
+
+  defp do_first_frequency_repeated_twice(
+         changes,
+         frequency \\ 0,
+         index \\ 0,
+         visited \\ MapSet.new()
+       )
+
+  defp do_first_frequency_repeated_twice(changes, frequency, index, visited) do
+    frequency = frequency + Enum.at(changes, rem(index, length(changes)))
+
+    if MapSet.member?(visited, frequency) do
+      frequency
+    else
+      do_first_frequency_repeated_twice(
+        changes,
+        frequency,
+        index + 1,
+        MapSet.put(visited, frequency)
+      )
+    end
   end
 end
--- test/day_01_chronal_calibration_test.exs
+++ test/day_01_chronal_calibration_test.exs
@@ -23,4 +23,32 @@ defmodule Adventofcode.Day01ChronalCalibrationTest do
       assert 531 = puzzle_input() |> resulting_frequency()
     end
   end
+
+  describe "first_frequency_repeated_twice/1" do
+    # TODO: Wrong expected result? Should be 1?
+    @tag :skip
+    test "+1, -1 first reaches 0 twice." do
+      input = "+1, -1"
+      assert 0 = input |> first_frequency_repeated_twice()
+    end
+
+    test "+3, +3, +4, -2, -4 first reaches 10 twice." do
+      input = "+3, +3, +4, -2, -4"
+      assert 10 = input |> first_frequency_repeated_twice()
+    end
+
+    test "-6, +3, +8, +5, -6 first reaches 5 twice." do
+      input = "-6, +3, +8, +5, -6"
+      assert 5 = input |> first_frequency_repeated_twice()
+    end
+
+    test "+7, +7, -2, -7, -4 first reaches 14 twice." do
+      input = "+7, +7, -2, -7, -4"
+      assert 14 = input |> first_frequency_repeated_twice()
+    end
+
+    test_with_puzzle_input do
+      assert 76787 = puzzle_input() |> first_frequency_repeated_twice()
+    end
+  end
 end
