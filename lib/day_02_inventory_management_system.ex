9f10fc1e0dd872b2024c6a8067dc87cf8052ac37
Albert Arvidsson <albert.arvidsson@gmail.com>
2018-12-02 06:52:24 +0100

    Add Day02InventoryManagementSystem.checksum/1

---
+++ lib/day_02_inventory_management_system.ex
@@ -0,0 +1,43 @@
+defmodule Adventofcode.Day02InventoryManagementSystem do
+  use Adventofcode
+
+  def checksum(input) do
+    input
+    |> String.split("\n")
+    |> Enum.map(&letter_counts/1)
+    |> count_box_ids_with_triplets_and_pairs
+  end
+
+  def letter_counts(input) do
+    input
+    |> String.graphemes()
+    |> Enum.group_by(& &1)
+    |> Enum.map(fn {letter, letters} -> {letter, length(letters)} end)
+    |> Enum.filter(fn {_, count} -> count == 2 || count == 3 end)
+    |> Enum.into(%{})
+  end
+
+  defp count_box_ids_with_triplets_and_pairs(letter_counts) do
+    count_pairs(letter_counts) * count_triplets(letter_counts)
+  end
+
+  defp count_pairs(letter_counts) do
+    letter_counts
+    |> Enum.filter(&has_pairs?/1)
+    |> Enum.count()
+  end
+
+  defp count_triplets(letter_counts) do
+    letter_counts
+    |> Enum.filter(&has_triplets?/1)
+    |> Enum.count()
+  end
+
+  defp has_pairs?(letter_counts) do
+    Enum.any?(letter_counts, fn {_, count} -> count == 2 end)
+  end
+
+  defp has_triplets?(letter_counts) do
+    Enum.any?(letter_counts, fn {_, count} -> count == 3 end)
+  end
+end
---
+++ test/day_02_inventory_management_system_test.exs
@@ -0,0 +1,64 @@
+defmodule Adventofcode.Day02InventoryManagementSystemTest do
+  use Adventofcode.FancyCase
+
+  import Adventofcode.Day02InventoryManagementSystem
+
+  describe "letter_counts/1" do
+    test "abcdef contains no letters that appear exactly two or three times." do
+      input = "abcdef"
+      assert %{} == input |> letter_counts()
+    end
+
+    test "bababc contains two a and three b, so it counts for both." do
+      input = "bababc"
+      assert %{"a" => 2, "b" => 3} = input |> letter_counts()
+    end
+
+    test "abbcde contains two b, but no letter appears exactly three times." do
+      input = "abbcde"
+      assert %{"b" => 2} = input |> letter_counts()
+    end
+
+    test "abcccd contains three c, but no letter appears exactly two times." do
+      input = "abcccd"
+      assert %{"c" => 3} = input |> letter_counts()
+    end
+
+    test "aabcdd contains two a and two d, but it only counts once." do
+      input = "aabcdd"
+      assert %{"a" => 2, "d" => 2} = input |> letter_counts()
+    end
+
+    test "abcdee contains two e." do
+      input = "abcdee"
+      assert %{"e" => 2} = input |> letter_counts()
+    end
+
+    test "ababab contains three a and three b, but it only counts once." do
+      input = "ababab"
+      assert %{"a" => 3, "b" => 3} = input |> letter_counts()
+    end
+  end
+
+  describe "checksum/1" do
+    # Of these box IDs, four of them contain a letter which appears exactly
+    # twice, and three of them contain a letter which appears exactly three
+    # times. Multiplying these together produces a checksum of 4 * 3 = 12
+    @input """
+    abcdef
+    bababc
+    abbcde
+    abcccd
+    aabcdd
+    abcdee
+    ababab
+    """
+    test "Multiplying these together produces a checksum of 4 * 3 = 12" do
+      assert 12 = @input |> checksum()
+    end
+
+    test_with_puzzle_input do
+      assert 6888 = puzzle_input() |> checksum()
+    end
+  end
+end
