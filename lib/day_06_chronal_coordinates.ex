2cdb9d8460d4ee1a5c12abc979bb5c773986e804
Albert Arvidsson <albert.arvidsson@gmail.com>
2018-12-06 06:32:51 +0100

    Implement Day06 parse_into_grid/1

--- lib/day_06_chronal_coordinates.ex
+++ lib/day_06_chronal_coordinates.ex
@@ -2,4 +2,18 @@ defmodule Adventofcode.Day06ChronalCoordinates do
   use Adventofcode
 
   def manhattan_distance({x1, y1}, {x2, y2}), do: abs(x1 - x2) + abs(y1 - y2)
+
+  def parse_coordinates(input) do
+    input
+    |> String.trim("\n")
+    |> String.split("\n")
+    |> Enum.map(&parse_coordinate/1)
+  end
+
+  defp parse_coordinate(coordinate) do
+    coordinate
+    |> String.split(", ")
+    |> Enum.map(&String.to_integer/1)
+    |> List.to_tuple()
+  end
 end
--- test/day_06_chronal_coordinates_test.exs
+++ test/day_06_chronal_coordinates_test.exs
@@ -3,6 +3,15 @@ defmodule Adventofcode.Day06ChronalCoordinatesTest do
 
   import Adventofcode.Day06ChronalCoordinates
 
+  @coordinates """
+  1, 1
+  1, 6
+  8, 3
+  3, 4
+  5, 5
+  8, 9
+  """
+
   describe "manhattan_distance/2" do
     test "distance between 0,0 and 1,1 is 2" do
       assert 2 = manhattan_distance({0, 0}, {1, 1})
@@ -14,6 +23,14 @@ defmodule Adventofcode.Day06ChronalCoordinatesTest do
 
     test "distance between 1,1 and 8,9 is 15 (=8-1+9-1)" do
       assert 15 = manhattan_distance({8, 9}, {1, 1})
+    end
+  end
+
+  @parsed_coordinates [{1, 1}, {1, 6}, {8, 3}, {3, 4}, {5, 5}, {8, 9}]
+
+  describe "parse_coordinates/1" do
+    test "returns grid of all parsed coordinates" do
+      assert @parsed_coordinates == @coordinates |> parse_coordinates
     end
   end
 end
